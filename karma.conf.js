bf0008b82deea901a60611e45b320a07b8827007
Alessandro Arnodo <alessandro@arnodo.net>
2017-05-29 17:34:27 +0200

    sasa

--- karma.conf.js
+++ karma.conf.js
@@ -1,15 +1,41 @@
 module.exports = function (config) {
+  var customLaunchers = {
+    sl_chrome: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Windows 7',
+      version: '35'
+    },
+    sl_firefox: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: '30'
+    },
+    sl_ios_safari: {
+      base: 'SauceLabs',
+      browserName: 'iphone',
+      platform: 'OS X 10.9',
+      version: '7.1'
+    },
+    sl_ie_11: {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 8.1',
+      version: '11'
+    }
+  }
+
   config.set({
     frameworks: ['mocha'],
-    reporters: ['mocha', 'coverage-istanbul'],
-    browsers: ['Chrome'],
+    reporters: ['mocha', 'coverage-istanbul', 'saucelabs'],
+    // browsers: ['Chrome'],
 
-    customLaunchers: {
-      Chrome_travis_ci: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
-    },
+    // customLaunchers: {
+    //   Chrome_travis_ci: {
+    //     base: 'Chrome',
+    //     flags: ['--no-sandbox']
+    //   }
+    // },
 
     files: ['test/**/*.spec.js'],
 
@@ -42,7 +68,14 @@ module.exports = function (config) {
     },
     webpackMiddleware: {
       noInfo: true
-    }
+    },
+    sauceLabs: {
+      testName: 'Web App Unit Tests'
+    },
+    customLaunchers: customLaunchers,
+    browsers: Object.keys(customLaunchers),
+    // reporters: ['dots', 'saucelabs'],
+    singleRun: true
   })
 
   if (process.env.TRAVIS) {
